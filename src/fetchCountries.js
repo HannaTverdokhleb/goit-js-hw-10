5b1a8318a6172205b71e7b92afa761898175bf9f
HannaTverdokhleb <84972134+HannaTverdokhleb@users.noreply.github.com>
2021-11-28 13:30:30 +0200

    Create fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,18 @@
+import Notiflix from 'notiflix';
+
+export default function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+      countryInfo.innerHTML = '';
+      countryList.innerHTML = '';
+    });
+}
